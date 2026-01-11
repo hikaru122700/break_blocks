8a88ceee1d68f20a22ef0096674dc036e6398da8
hikaru122700 <hikaru122700@gmail.com>
2026-01-11 21:27:45 +0900

    auto-commit: 2026-01-11 21:27:44

--- ai_training/env/__init__.py
+++ ai_training/env/__init__.py
@@ -1,3 +1,4 @@
 from .constants import *
 from .game_simulation import Ball, Paddle, Block, DurableBlock, PowerUp, GameSimulation
 from .breakout_env import BreakoutEnv, CurriculumBreakoutEnv, EasyBreakoutEnv
+from .simple_env import SimpleBreakoutEnv
